db93cc99fb0a2fc5a4ce47263849a2c3d2769571
stuartcmehrens <stuart.mehrens@datadoghq.com>
2023-03-19 15:58:53 -0400

    fix paths

--- datadog/deploy.sh
+++ datadog/deploy.sh
@@ -1,9 +1,9 @@
 envsubst < ./values.yaml
-cat values.yaml
+cat ./values.yaml
 
 helm repo add datadog https://helm.datadoghq.com
 helm repo update
 helm upgrade datadog-agent \
--f values.yaml \
+-f ./values.yaml \
 --install \
 datadog/datadog
