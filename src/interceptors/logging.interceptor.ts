cc02f134ad0e1edc7c5cf3795215e8176b8f5bac
stuartcmehrens <stuart.mehrens@datadoghq.com>
2023-03-20 23:21:38 -0400

    add interceptor

---
+++ src/interceptors/logging.interceptor.ts
@@ -0,0 +1,59 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  constructor(private readonly logger: Logger) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const controller = context.getClass().name;
+    const action = context.getHandler().name;
+    const httpContext = context.switchToHttp();
+    const { method, path, params } = httpContext.getRequest<Request>();
+
+    // this seems janky
+    httpContext.getResponse<Response>()
+      .addListener('close', () => {
+        this.logger.verbose({
+          controller,
+          action,
+          method,
+          path,
+          params,
+          message: 'serving response.',
+          statusCode: httpContext.getResponse<Response>().statusCode,
+        });
+      });
+
+    this.logger.debug({
+        controller,
+        action,
+        method,
+        path,
+        params,
+        message: 'incoming request.',
+      },
+      LoggingInterceptor.name,
+    );
+    
+    return next
+      .handle()
+      .pipe(
+        tap({
+          error: err => {
+            this.logger.error({
+              err,
+            });
+          },
+        })
+      );
+  }
+}
--- src/logging/logger.options.ts
+++ src/logging/logger.options.ts
@@ -3,7 +3,7 @@ import { format, transports, LoggerOptions } from 'winston';
 export function getLoggerOptions(): LoggerOptions {
   return {
     level: 'debug',
-    format: format.combine(format.json(), format.timestamp()),
+    format: format.combine(format.timestamp(), format.json()),
     transports: [new transports.Console()],
     handleExceptions: true,
   };
