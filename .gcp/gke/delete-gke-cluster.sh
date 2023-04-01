6591c010063744432603a5757a41ecd8ad0a71a0
stuartcmehrens <stuart.mehrens@datadoghq.com>
2023-04-01 02:16:23 -0400

    remove quotes in delete

--- .gcp/gke/delete-gke-cluster.sh
+++ .gcp/gke/delete-gke-cluster.sh
@@ -4,7 +4,7 @@
 gcloud config set disable_prompts true
 gcloud config set core/verbosity error
 
-EXISTING_SA="\"$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com\""
+EXISTING_SA="$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com"
 
 # delete service account
 SERVICE_ACCOUNTS=$(gcloud iam service-accounts list \
@@ -16,7 +16,7 @@ SERVICE_ACCOUNTS=$(gcloud iam service-accounts list \
 echo "found service accounts $SERVICE_ACCOUNTS"
 for SERVICE_ACCOUNT in $SERVICE_ACCOUNTS
 do
-    if [ $SERVICE_ACCOUNT = $EXISTING_SA ]
+    if [ $SERVICE_ACCOUNT = "\"$EXISTING_SA\"" ]
     then
         echo "deleting service account $SERVICE_ACCOUNT"
         gcloud iam service-accounts delete $EXISTING_SA --project $PROJECT_ID
@@ -33,7 +33,7 @@ CLUSTERS=$(gcloud container clusters list \
 echo "found clusters $CLUSTERS"
 for CLUSTER in $CLUSTERS
 do
-    if [ $CLUSTER = \"$CLUSTER_NAME\" ]
+    if [ $CLUSTER = "\"$CLUSTER_NAME\"" ]
     then
         echo "deleting cluster $CLUSTER"
         gcloud container clusters delete $CLUSTER_NAME --region $REGION
