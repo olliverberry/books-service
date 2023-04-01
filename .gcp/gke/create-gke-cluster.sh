bb0950593ad40dc43c7438c37b0ccf496e64749c
stuartcmehrens <122312364+stuartcmehrens@users.noreply.github.com>
2023-04-01 01:49:24 -0400

    modified scripts (#28)

--- .gcp/gke/create-gke-cluster.sh
+++ .gcp/gke/create-gke-cluster.sh
@@ -2,18 +2,38 @@
 
 # disable prompts
 gcloud config set disable_prompts true
+gcloud config set core/verbosity error
 
-# create service account
-gcloud iam service-accounts create $SA_NAME \
-    --project $PROJECT_ID
+EXISTING_SA="\"$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com\""
 
-# give service account iam role
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-    --member "serviceAccount:$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
-    --role roles/container.nodeServiceAccount
+# check to see if service account exists; if not create it
+output=$(gcloud iam service-accounts list \
+    --project $PROJECT_ID \
+    --filter "email=$EXISTING_SA" \
+    --format json | jq)
+if [ "$output" = "[]" ]
+then
+    echo "creating service account $SA_NAME"
+    gcloud iam service-accounts create $SA_NAME \
+        --project $PROJECT_ID
 
-# create the clusters
-gcloud container clusters create-auto $CLUSTER_NAME \
-    --service-account "$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
-    --region $REGION
+    echo "attaching iam role to service account"
+    # give service account iam role
+    gcloud projects add-iam-policy-binding $PROJECT_ID \
+        --member "serviceAccount:$EXISTING_SA" \
+        --role roles/container.nodeServiceAccount
+fi
+
+# check to see if cluster exists; if not create it
+output=$(gcloud container clusters list \
+    --region $REGION \
+    --format json \
+    --filter "name=$CLUSTER_NAME" | jq)
+if [ "$output" = "[]" ]
+then
+    echo "creating cluster $CLUSTER_NAME"
+    gcloud container clusters create-auto $CLUSTER_NAME \
+        --project $PROJECT_ID \
+        --service-account "$EXISTING_SA"
+fi
     
--- .gcp/gke/delete-gke-cluster.sh
+++ .gcp/gke/delete-gke-cluster.sh
@@ -2,11 +2,40 @@
 
 # disable prompts
 gcloud config set disable_prompts true
+gcloud config set core/verbosity error
+
+EXISTING_SA="\"$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com\""
 
 # delete service account
-gcloud iam service-accounts delete $SA_NAME \
-    --project $PROJECT_ID
+SERVICE_ACCOUNTS=$(gcloud iam service-accounts list \
+    --project $PROJECT_ID \
+    --format json \
+    --filter "email=$EXISTING_SA" \
+    | jq '.[].email?')
 
-# delete the cluster
-gcloud container clusters delete $CLUSTER_NAME \
-    --region $REGION
+echo "found service accounts $SERVICE_ACCOUNTS"
+for SERVICE_ACCOUNT in $SERVICE_ACCOUNTS
+do
+    if [ $SERVICE_ACCOUNT = $EXISTING_SA ]
+    then
+        echo "deleting service account $SERVICE_ACCOUNT"
+        gcloud iam service-accounts delete $EXISTING_SA --project $PROJECT_ID
+    fi
+done
+
+# delete cluster
+CLUSTERS=$(gcloud container clusters list \
+    --region $REGION \
+    --format json \
+    --filter "name=$CLUSTER_NAME" \
+    | jq '.[].name?')
+
+echo "found clusters $CLUSTERS"
+for CLUSTER in $CLUSTERS
+do
+    if [ $CLUSTER = \"$CLUSTER_NAME\" ]
+    then
+        echo "deleting cluster $CLUSTER"
+        gcloud container clusters delete $CLUSTER_NAME --region $REGION
+    fi
+done
