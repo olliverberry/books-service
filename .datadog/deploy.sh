e8817d5588f08131c6cce9bb1741dc95564f2c38
stuartcmehrens <122312364+stuartcmehrens@users.noreply.github.com>
2023-04-18 22:21:29 -0400

    Feature/google cloud run (#34)
    * add grc deploy
    
    * add port

--- .datadog/deploy.sh
+++ .datadog/deploy.sh
@@ -2,9 +2,9 @@
 helm repo add datadog https://helm.datadoghq.com
 helm repo update
 helm upgrade datadog-agent \
--f $VALUES_FILE_PATH \
---set datadog.apiKey=$DATADOG_API_KEY \
---set datadog.appKey=$DATADOG_APP_KEY \
---set datadog.clusterName=$CLUSTER_NAME \
---install \
-datadog/datadog
+    -f $VALUES_FILE_PATH \
+    --set datadog.apiKey=$DATADOG_API_KEY \
+    --set datadog.appKey=$DATADOG_APP_KEY \
+    --set datadog.clusterName=$CLUSTER_NAME \
+    --install \
+    datadog/datadog
